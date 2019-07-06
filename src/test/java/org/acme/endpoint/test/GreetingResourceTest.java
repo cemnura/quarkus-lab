93b3e6e3b25e9240f80c350eab5209c1054843bc
cem.nura <cem.nura@gmail.com>
2019-07-06 15:54:15 +0300

    Added test

---
+++ src/test/java/org/acme/endpoint/test/GreetingResourceTest.java
@@ -0,0 +1,22 @@
+package org.acme.endpoint.test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/test/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello!!!"));
+    }
+}
