4d1dcbafddc643a16bb8ca31cb196c31dece9c92
cem.nura <cem.nura@gmail.com>
2019-07-06 15:42:45 +0300

    Added porject template

---
+++ src/main/java/org/acme/service/HelloEndpoint.java
@@ -0,0 +1,17 @@
+package org.acme.service;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@RequestScoped
+@Path("/test")
+public class HelloEndpoint {
+
+    @GET
+    @Path("/hello")
+    public String hello()
+    {
+        return "Hello!!!";
+    }
+}
